e5366f5ad24aabc9d5ac31dffdedea87fdffec85
ManuelJervi98 <59656834+ManuelJervi98@users.noreply.github.com>
2020-03-09 00:00:58 -0400

    Actualizacion y testeo

--- AsignacionAPI/apiRequest.js
+++ AsignacionAPI/apiRequest.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() { 
+document.addEventListener("DOMContentLoaded", function () {
     //////////// Opcion 1 usar Ajax(Jquery) para hacer el request usando JSON ////////////////////
     /*$.ajax({
         url: "http://api.openweathermap.org/data/2.5/weather?q=London&appid=76fa9eba3db469038ec999a9400a02eb",
@@ -32,30 +32,27 @@ document.addEventListener("DOMContentLoaded", function() {
         });*/
 
     //////////// Opcion 3 XMLHttpRequest ////////////////////
-
-    
-    
-
     var request = new XMLHttpRequest();
-        request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=76fa9eba3db469038ec999a9400a02eb', false);
-        request.send(null);
-        if (request.status === 200) {
-            var testo = document.querySelector('.testo');
-            testo.textContent = request.responseText;
-        } else {
-            alert(request.responseText);
+
+    request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=Oruro&units=celsius&appid=76fa9eba3db469038ec999a9400a02eb', false);
+    request.send(); //Hasta aqui envio el request
+
+    if (request.status === 200) {
+        var texto = document.querySelector('.texto');
+
+        if (texto) {
+            texto.textContent = request.responseText;
         }
 
-        var botoncito = document.querySelector('.botonicto');
+        var responseJson = JSON.parse(request.responseText); //Convertir la respuesta de texto a JSON
 
-        botoncito.onclick = function(){
-            console.log('he sido clickeado')
-        };
+        var valor = responseJson.main.temp;
 
-
-        
-
-        
-    
+        document.querySelector('.weather-icon').src = "http://openweathermap.org/img/w/" + responseJson.weather[0].icon + ".png";
+        document.querySelector('.weather-value').textContent = valor;
+        document.querySelector('.city').textContent = responseJson.name;
+    } else {
+        alert(request.responseText);
+    }
 });
 
